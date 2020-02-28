97d7940f2e59688b78c151d102a744b291f1ad9d
madawei2699 <madawei2699@gmail.com>
2020-02-28 17:50:50 +0800

    WIP

--- sync_s3.sh
+++ sync_s3.sh
@@ -2,3 +2,5 @@
 
 aws s3 sync --acl public-read tg_media s3://img.bmpi.dev/tg_media
 aws s3 sync --acl public-read html s3://tg.bmpi.dev/
+
+aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths "/*"
